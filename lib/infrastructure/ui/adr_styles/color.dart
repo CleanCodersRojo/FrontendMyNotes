9b2e86101efda4c8ad281e3a348d5e2d4bc73444
Adrian Lopez Larrad <alopezl.19@est.ucab.edu.ve>
2023-06-14 17:30:57 -0400

    Add file styles and class color.dart
    Agregando la carpeta de estilos para los colores de las interfaces

---
+++ lib/infrastructure/ui/adr_styles/color.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+///This file is used to manage the colors of the application.
+
+//  const colorPrimary = Color.fromARGB(255, 41, 205, 191);
+// const colorSecondary = Color(0xFFcecece);
+//  const colorActive = Color.fromARGB(255, 4, 159, 255);
+//  const colorActive = Color.fromARGB(255, 92, 190, 251);
+//  const colorInactive = Color(0xFF7b7c7c);
+//  const colorError = Color(0xFFC5032B);
+//  const colorGreen = Color.fromARGB(255, 14, 186, 14);
+//  const colorYellow = Color.fromARGB(255, 212, 197, 28); 
+
+
+const colorBackgroundOrange = Color.fromARGB(255,181,98,1);
+const colorBackgroundBlue = Color.fromARGB(255, 92, 190, 251);
+const colorBlack = Color(0xFF000B30);
+const colorWhite = Color(0xFFFFFFFF);
+const colorRed = Color(0xFFC5032B);
+const colorGreen = Color.fromARGB(255, 14, 186, 14);
+const colorGrey = Color(0xFF7b7c7c);
