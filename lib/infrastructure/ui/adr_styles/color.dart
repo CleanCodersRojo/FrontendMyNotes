90292eb5879f102503fcd7413d3d406bfdee2086
AntoniobB09 <antblanco@gmail.com>
2023-06-14 20:41:13 -0400

    vista-login

--- lib/infrastructure/ui/adr_styles/color.dart
+++ lib/infrastructure/ui/adr_styles/color.dart
@@ -9,10 +9,23 @@ import 'package:flutter/material.dart';
 //  const colorInactive = Color(0xFF7b7c7c);
 //  const colorError = Color(0xFFC5032B);
 //  const colorGreen = Color.fromARGB(255, 14, 186, 14);
-//  const colorYellow = Color.fromARGB(255, 212, 197, 28); 
+//  const colorYellow = Color.fromARGB(255, 212, 197, 28);
 
+const noteThemePink50 = Color.fromARGB(255, 236, 240, 40);
+const noteThemePink100 = Color.fromARGB(255, 229, 252, 165);
+const noteThemePink300 = Color(0xFFFBB8AC);
+const noteThemePink400 = Color(0xFFEAA4A4);
 
-const colorBackgroundOrange = Color.fromARGB(255,181,98,1);
+const noteThemeBrown900 = Color.fromARGB(255, 35, 132, 212);
+
+const noteThemeErrorRed = Color(0xFFC5032B);
+
+const noteThemeSurfaceWhite = Color(0xFFFFFBFA);
+const noteThemeBackgroundWhite = Colors.white;
+
+const noteThemePurple = Color(0xFF5D1049);
+
+const colorBackgroundOrange = Color.fromARGB(255, 181, 98, 1);
 const colorBackgroundBlue = Color.fromARGB(255, 92, 190, 251);
 const colorBlack = Color(0xFF000B30);
 const colorWhite = Color(0xFFFFFFFF);
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,79 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/infrastructure/ui/adr_notes/listNotes.dart';
-import 'package:flutter_application_1/infrastructure/ui/adr_notes/saveNotes.dart';
 
-void main() => runApp(MyApp());
+import 'infrastructure/ui/adr_notes/saveNotes.dart';
+import 'infrastructure/ui/adr_styles/color.dart';
+import 'infrastructure/ui/login.dart';
 
-class MyApp extends StatelessWidget{
+void main() => runApp(const NoteApp());
+
+class NoteApp extends StatelessWidget {
+  const NoteApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       title: 'Material App',
-       home: SaveNotes(),
+      title: 'MyNote',
+      initialRoute: '/login',
+      routes: {
+        '/login': (BuildContext context) => const LoginPage(),
+        '/': (BuildContext context) => SaveNotes(),
+      },
+      theme: _noteTheme,
     );
   }
+}
+
+final ThemeData _noteTheme = _buildNoteTheme();
+
+ThemeData _buildNoteTheme() {
+  final ThemeData base = ThemeData.light(useMaterial3: true);
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: noteThemePink100,
+      onPrimary: noteThemeBrown900,
+      secondary: noteThemeBrown900,
+      error: noteThemeErrorRed,
+    ),
+    textTheme: _buildNoteTextTheme(base.textTheme),
+    textSelectionTheme: const TextSelectionThemeData(
+      selectionColor: noteThemePink100,
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      border: OutlineInputBorder(),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(
+          width: 2.0,
+          color: noteThemeBrown900,
+        ),
+      ),
+      floatingLabelStyle: TextStyle(
+        color: noteThemeBrown900,
+      ),
+    ),
+  );
+}
+
+TextTheme _buildNoteTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        headlineSmall: base.headlineSmall!.copyWith(
+          fontWeight: FontWeight.w500,
+        ),
+        titleLarge: base.titleLarge!.copyWith(
+          fontSize: 18.0,
+        ),
+        bodySmall: base.bodySmall!.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14.0,
+        ),
+        bodyLarge: base.bodyLarge!.copyWith(
+          fontWeight: FontWeight.w500,
+          fontSize: 16.0,
+        ),
+      )
+      .apply(
+        fontFamily: 'Rubik',
+        displayColor: noteThemeBrown900,
+        bodyColor: noteThemeBrown900,
+      );
 }
