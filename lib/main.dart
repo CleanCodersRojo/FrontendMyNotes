87058e9c7cc84da2f03db0d50b1902e33cdb1043
Adrian Lopez Larrad <alopezl.19@est.ucab.edu.ve>
2023-07-03 00:00:14 -0400

    Update main.dart
    Agregando rutas para el manejo de las vistas

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/screens/edit.dart';
 import 'package:note_app/screens/home.dart';
+import 'package:note_app/screens/ocr.dart';
+import 'package:note_app/screens/speech.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -10,9 +13,27 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: MyApp(),
+    );
+  }
+}
+
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/",
+      routes: {
+        "/" : (context) => HomeScreen(),
+        "/editar": (context) => EditScreen(),
+        "/speech": (context) => SpeechScreen(),
+        "/ocr": (context) => ocrScreen()
+      }
     );
   }
 }
