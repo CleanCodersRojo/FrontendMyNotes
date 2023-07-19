6027a9ea3c359e70fa733caf77edce0273314d32
Adrian Lopez Larrad <alopezl.19@est.ucab.edu.ve>
2023-07-19 13:13:10 -0400

    Update information for the note
    Actualizando la información de la nota para mostrar sus características

--- lib/screens/informationNote.dart
+++ lib/screens/informationNote.dart
@@ -1,48 +1,133 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../models/note.dart';
 import 'edit.dart';
+import 'geolocator.dart';
 
 class InformationScreen extends StatelessWidget{
-  // final String text;
+
   final Note? nota;
-  const InformationScreen({superKey, this.nota});
+  InformationScreen({superKey, this.nota});
 
   @override
   Widget build(BuildContext context){  
     
-    return Scaffold(
-    body: Padding(
-      padding: const EdgeInsets.all(30.0),
-      
-      child: Column(
-        children: [
-          Row(
+    final TextStyle estiloTexto = TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.bold,
+      color: Colors.white,
+    );
+    final DateFormat formatoFecha = DateFormat('d/M/yyyy, h:mma');
 
-            children: [
-              IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
+    return Scaffold(
+      backgroundColor: Colors.grey.shade900,
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);  
+                  },
+                  padding: EdgeInsets.all(0),
+                  icon: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade800.withOpacity(0.8),
+                      borderRadius: BorderRadius.circular(10)),
+                    child: const Icon(
+                      Icons.arrow_back_ios_new,
+                      color: Colors.white,
+                    ),
+                  )
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Text(
+                    'Fecha de creación',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                    ),
+                  ),
+                  Text(
+                    nota != null ? formatoFecha.format(nota!.fechaCreacion) : 'Fecha Creacion',
+                    style: estiloTexto,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Fecha de actualización',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                    ),
+                  ),
+                  Text(
+                    nota != null ? formatoFecha.format(nota!.fechaActualizacion) : 'Fecha Actualizacion',
+                    style: estiloTexto,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Latitud',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                    ),
+                  ),
+                  Text(
+                    nota?.latitud?.toString() ?? 'No hay latitud',
+                    style: estiloTexto,
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Longitud',
+                    style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white
+                    ),
+                  ),
+                  Text(
+                    nota?.altitud?.toString() ?? 'No hay longitud',
+                    style: estiloTexto,
+                  ),
+                  SizedBox(height: 20),
+                  FloatingActionButton(
+                    
+                    onPressed: () async {
+                      if(nota!.altitud != null && nota!.latitud != null){
+                        Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => LocationMapView(nota: nota)),
+                      );
+                      }else{
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                            content: Text('El mapa no puede ser mostrado'),
+                            duration: Duration(seconds: 3),
+                            ),
+                        );
+                      }
                       
                     },
-                    padding: EdgeInsets.all(0),
-                    icon: Container(
-                      width: 40,
-                      height: 40,
-                      decoration: BoxDecoration(
-                          color: Colors.grey.shade800.withOpacity(0.8),
-                          borderRadius: BorderRadius.circular(10)),
-                      child: const Icon(
-                        Icons.arrow_back_ios_new,
-                        color: Colors.white,
-                      ),
-                    )),
-            ],
-          )
-        ],
-      )
-    ),
-    
-  );
+                    backgroundColor: Colors.green,
+                    child: const Icon(Icons.maps_ugc, color: Colors.white),
+                  ),
+                ],
+              ),
+            )
+          ],
+        )
+      ),
+    );
   }
 }
-
