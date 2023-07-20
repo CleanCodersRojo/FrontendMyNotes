2dd9e798480a7ffa94bb5df0af28512fd60800ef
Adrian Lopez Larrad <alopezl.19@est.ucab.edu.ve>
2023-07-20 02:49:15 -0400

    Update main
    Actualizando el main para que se permita utilizar la bd de manera global

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:note_app/screens/edit.dart';
 import 'package:note_app/screens/home.dart';
 import 'package:note_app/screens/ocr.dart';
 import 'package:note_app/screens/speech.dart';
+import 'package:note_app/storage/local_storage.dart';
 
-void main() {
+final storage = LocalStorage();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await storage.open();
   runApp(MainApp());
 }
 
